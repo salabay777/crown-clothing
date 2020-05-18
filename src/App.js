1c3cdbc2b34cac79f947a10883207ac5be3f7edc
unknown <salabay2003@gmail.com>
2020-05-18 19:31:02 +0300

    implemented redirect to homepage if user is logged in

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, {useEffect} from 'react';
 import {connect} from 'react-redux';
-import {Switch, Route} from 'react-router-dom';
+import {Switch, Route, Redirect} from 'react-router-dom';
 import './App.css';
 
 import {setCurrentUser} from './redux/user/userActions';
@@ -12,7 +12,7 @@ import HomePage from './pages/homepage/HomePage';
 import ShopPage from './pages/shop/ShopPage';
 import SigninAndSignup from './pages/sign-in-and-sign-up/SigninAndSignup';
 
-function App({setCurrentUser}) {
+function App({currentUser, setCurrentUser}) {
 	let unsubscribeFromAuth = null;
 
 	useEffect(() => {
@@ -44,14 +44,21 @@ function App({setCurrentUser}) {
 		    <Switch>
 		    	<Route exact path ='/' component={HomePage} />
 		    	<Route path='/shop' component={ShopPage} />
-		    	<Route path='/signin' component={SigninAndSignup} />
+		    	<Route exact path='/signin' render={() => currentUser 
+		    		? <Redirect to='/' /> 
+		    		: <SigninAndSignup /> 
+		    	} />
 		    </Switch>
 	    </>
     );
 }
 
+const mapStateToProps = state => ({
+	currentUser: state.user.currentUser
+});
+
 const mapDispatchToProps = dispatch => ({
 	setCurrentUser: user => dispatch(setCurrentUser(user))
 });
 
-export default connect(null, mapDispatchToProps)(App);
+export default connect(mapStateToProps, mapDispatchToProps)(App);
--- src/redux/user/userReducer.js
+++ src/redux/user/userReducer.js
@@ -1,10 +1,12 @@
+import {userActionsTypes} from './userTypes';
+
 const initialState = {
 	currentUser: null
 };
 
 export const userReducer = (state = initialState, action) => {
 	switch(action.type){
-		case 'SET_CURRENT_USER':
+		case userActionsTypes.SET_CURRENT_USER:
 			return {
 				...state,
 				currentUser: action.payload
